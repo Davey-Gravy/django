a3cc51b7730d31b205d2c4916a5b3fc558772eba
Davis Cole <daveygravy@LEBFTST14.win.ansys.com>
2022-10-31 13:14:09 -0400

    initial

---
+++ mysite/main/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import ToDoList, Item
+from .forms import CreateNewList
+
+# Create your views here.
+def index(response, id):
+    lst = ToDoList.objects.get(id=id)
+    if response.method == "POST":
+        print(response.POST)
+        # if using the save button, update checkbox status
+        if response.POST.get('save'):
+            for item in lst.item_set.all():
+                if response.POST.get(f'c{item.id}') == 'clicked':
+                    item.complete = True
+                else:
+                    item.complete = False
+                item.save()
+        # if using add item button, create new item, set text as input
+        elif response.POST.get('newItem'):
+            txt = response.POST.get('new')
+            if len(txt) > 2:
+                lst.item_set.create(text=txt, complete=False)
+            else:
+                print('invalid')
+    
+    return render(response, 'main/list.html', {'lst':lst})
+
+def home(response):
+    return render(response, 'main/home.html', {})
+
+def create(response):
+    if response.method == "POST":       
+        form = CreateNewList(response.POST)
+
+        # if form string is valid,
+        # clean name, create todo list
+        if form.is_valid():
+            n = form.cleaned_data['name']
+            t = ToDoList(name=n)
+            t.save()
+
+        # if form is post request,
+        # redirect to newly created list
+        return HttpResponseRedirect(f'/{t.id}')
+
+    else:
+        form = CreateNewList()
+    
+    return render(response, 'main/create.html', {'form':form})
