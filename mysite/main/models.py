09f88c4819fb7e58d0dbf300cb4f459bb78b6f64
Davis Cole <daveygravy17@gmail.com>
2022-11-01 14:04:13 -0400

    cleanup

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -3,13 +3,18 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class ToDoList(models.Model):
+    # Links ToDoList with User
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # properties
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
 class Item(models.Model):
+    # links Item with ToDoList
     todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    # properties
     text = models.CharField(max_length=300)
     complete = models.BooleanField()
 
