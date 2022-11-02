974f6b1c82037191570e7247bd3e47ba86fff5a4
Davis Cole <daveygravy17@gmail.com>
2022-11-02 12:56:20 -0400

    added 'home/' url

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("<int:id>", views.index, name='Index'),
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('create/', views.create, name='Create'),
     path('view/', views.view, name='view')
 ]
